19c969f87a6da867c25cc9852a20485612269197
James Fosburgh <jwfosburgh@gmail.com>
2024-01-26 20:10:04 -0500

    implemented todo editing

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ var (
 
 type todoitem struct {
 	Checked   bool
+	Editing   bool
 	Label     string
 	LabelId   string
 	Id        string
@@ -44,6 +45,34 @@ func parseIdFromURL(r *http.Request) int {
 	id := path.Base(r.URL.String())
 	intId, _ := strconv.Atoi(id)
 	return intId
+}
+
+func (cfg *dataconfig) handleEdit(w http.ResponseWriter, r *http.Request) {
+	intId := parseIdFromURL(r)
+	itemSlice := *cfg.TDItems
+	switch r.Method {
+	case http.MethodGet:
+		itemSlice[intId].Editing = true
+
+		cfg.Templates.ExecuteTemplate(w, "todo-item.html", itemSlice[intId])
+	case http.MethodPut:
+		type todo struct {
+			Value string `json:"todo"`
+		}
+
+		newTodo := todo{}
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&newTodo)
+		if err != nil {
+			fmt.Printf("Err decoding json: %v\n", err)
+		}
+
+		itemSlice[intId].Label = newTodo.Value
+		itemSlice[intId].Editing = false
+
+		cfg.Templates.ExecuteTemplate(w, "todo-item.html", itemSlice[intId])
+	}
+	cfg.TDItems = &itemSlice
 }
 
 func (cfg *dataconfig) handleToggle(w http.ResponseWriter, r *http.Request) {
@@ -63,30 +92,6 @@ func (cfg *dataconfig) handleToggle(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// func (cfg *dataconfig) handleTodo(w http.ResponseWriter, r *http.Request) {
-// 	switch r.Method {
-// 	case http.MethodDelete:
-// 		intId := parseIdFromURL(r)
-// 		itemSlice := []todoitem{}
-// 		for i, todo := range *cfg.TDItems {
-// 			if i != intId {
-// 				newTodo := todoitem{
-// 					Id:        fmt.Sprintf("%d", i),
-// 					Label:     todo.Label,
-// 					LabelId:   fmt.Sprintf("label_%d", i),
-// 					Checked:   todo.Checked,
-// 					Class:     todo.Class,
-// 					InputName: fmt.Sprintf("checkbox_%d", i),
-// 				}
-// 				itemSlice = append(itemSlice, newTodo)
-// 			}
-// 		}
-//
-// 		cfg.Templates.ExecuteTemplate(w, "todo-list.html", itemSlice)
-// 		cfg.TDItems = &itemSlice
-// 	}
-// }
-
 func (cfg *dataconfig) handleTodo(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
@@ -105,6 +110,7 @@ func (cfg *dataconfig) handleTodo(w http.ResponseWriter, r *http.Request) {
 
 		newList := append(*cfg.TDItems, todoitem{
 			Checked:   false,
+			Editing:   false,
 			Label:     newTodo.Value,
 			LabelId:   fmt.Sprintf("label_%d", numTodos),
 			Id:        fmt.Sprintf("%d", numTodos),
@@ -125,6 +131,7 @@ func (cfg *dataconfig) handleTodo(w http.ResponseWriter, r *http.Request) {
 					Label:     todo.Label,
 					LabelId:   fmt.Sprintf("label_%d", i),
 					Checked:   todo.Checked,
+					Editing:   false,
 					Class:     todo.Class,
 					InputName: fmt.Sprintf("checkbox_%d", i),
 				}
@@ -165,7 +172,8 @@ func main() {
 	r.HandleFunc("/", config.handleIndex)
 	r.Handle("/css/output.css", http.FileServer(http.FS(css)))
 	r.HandleFunc("/todos/", config.handleTodo)
-	r.HandleFunc("/toggle/", config.handleToggle)
+	r.HandleFunc("/todos/toggle/", config.handleToggle)
+	r.HandleFunc("/todos/edit/", config.handleEdit)
 
 	s := http.Server{
 		Addr:    addr,
